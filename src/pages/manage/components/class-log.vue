3027a66835f90d2aac6992f3a2ce163defb3ca62
j <1336679971@qq.com>
2024-03-26 22:17:55 +0800

    navigator

--- src/pages/manage/components/class-log.vue
+++ src/pages/manage/components/class-log.vue
@@ -1,10 +1,76 @@
 <script lang="ts" setup>
+import { ref } from 'vue'
+const popup = ref<UniHelper.UniPopupInstance>()
+const str = `《原神》是由中国公司 miHoYo 开发和发布的一款开放世界冒险游戏。游戏首次在 2020 年发布，可以在多个平台上玩，包括 Windows、PlayStation 4、iOS、Android，以及 Nintendo Switch。
+
+在《原神》中，玩家扮演的是一位被称为"旅行者"的角色，他在多个世界中旅行，寻找他的失散兄弟或姐妹。游戏目前包含了几个大的区域，每个区域都有自己的文化和风景，以及独特的任务和故事。
+
+游戏的战斗系统基于角色的元素属性，通过组合不同元素属性可以产生各种效果。此外，游戏还包含了解谜和探索元素，玩家可以在开放世界中自由探索，寻找隐藏的宝藏和秘密。
+
+《原神》以其精美的画面、丰富的世界观和深度的角色系统受到了玩家的热烈欢迎。`;
 </script>
 
 <template>
-  <view style="background-color: aliceblue; grid-template:100px /100px;"><view>21321312312312312312312321312321321312312321</view></view>
+  <uni-popup ref="popup" type="dialog">
+    <uni-popup-dialog  :showClose="false" title="详细内容">
+      <scroll-view :scroll-y="true" style="height: 400px;"> {{ str }} </scroll-view>
+    </uni-popup-dialog>
+  </uni-popup>
+  
+  <view class="AllContain">
+    <view class="TextContain">
+      <view class="index">作者</view>
+      <view class="index">标题</view>
+      <view class="index">时间</view>
+      <view class="title">原神</view>
+      <view class="content" @click="popup?.open?.()">《原神》是由中国公司 miHoYo 开发和发布的一款开放世界冒险游戏。</view>
+      <view class="time">2024/03/26</view>
+      <view class="title">原神</view>
+      <view class="content" @click="popup?.open?.()">《原神》是由中国公司 miHoYo 开发和发布的一款开放世界冒险游戏。</view>
+      <view class="time">2024/03/26</view>
+    </view>
+
+    <uni-pagination style="margin: 0px 10px 5px;" title="标题文字" :total="50" :current="2"></uni-pagination>
+  </view>
+
+
+  
 </template>
 
 <style lang="scss" scoped>
+.AllContain{
+  display: grid;
+  margin: 10px;
+  margin-top: 20px;
+  background-color: white;
+  grid-template-columns : 1fr;
+  grid-auto-rows: auto;
+  border-radius: 10px;
+}
 
+.TextContain{
+  display: grid;
+  grid-template-columns: 1fr 2fr 1fr;
+  grid-auto-rows: auto;
+  row-gap:10px;
+}
+
+.index{
+  justify-self:center;
+  align-self:center;
+}
+
+.title{
+  align-self:center;
+  justify-self: center;
+}
+
+.content{
+  color:blue;
+}
+
+.time{
+  align-self:center;
+  justify-self: center;
+}
 </style>
--- src/pages/manage/manage.vue
+++ src/pages/manage/manage.vue
@@ -1,19 +1,25 @@
 <script lang="ts" setup>
 import classLog from '@/pages/manage/components/class-log.vue'
 import notification from '@/pages/manage/components/notification.vue'
+import { ref } from 'vue'
+const nowTitle = ref<string>('信息通知')
 </script>
 
 <template>
-  <view class="select">
-    <button style="background-color: #FFCCE5;">
-      信息通知
-    </button>
-    <button style="background-color: #FFFFCC;">
-      班务日志
-    </button>
+  <view class="head">
+    <h2 style="justify-self: center;align-self: center;"> {{nowTitle}} </h2>
+    <view class="select">
+      <button style="background-color: #FFCCE5;" size="mini" @click="nowTitle = '信息通知'">
+        信息通知
+      </button>
+      <button style="background-color: #FFFFCC;" size="mini" @click="nowTitle = '班务日志'">
+        班务日志
+      </button>
+    </view>
   </view>
   
-  <view v-if="true">
+  
+  <view v-if="nowTitle === '信息通知'">
     <classLog/>
   </view>
   <view v-else>
@@ -22,11 +28,18 @@ import notification from '@/pages/manage/components/notification.vue'
 </template>
 
 <style lang="scss" scoped>
+.head{
+  margin: 20px 40px 20px;
+  border-radius: 10px;
+  background-color: white;
+  display: grid;
+  grid-template: 30px 40px / 1fr;
+}
+
 .select{
-  margin:20px;
   border-radius: 10px;
   display:grid;
-  grid-template: 60px/repeat(2,1fr);
+  grid-template: 40px/repeat(2,1fr);
   align-items:center;
   justify-items:center;
 }
